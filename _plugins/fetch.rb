34ff46ffe388c7b1768c8d2c510068f729f81fd5
kenwilcox <wilcoxk@gmail.com>
2014-10-04 12:52:17 -0600

    misc file stuff

--- _plugins/fetch.rb
+++ _plugins/fetch.rb
@@ -3,10 +3,10 @@ require 'open-uri'
 
 module Jekyll
   class FetchGenerator < Generator
-    safe true
+    safe true # not sure if this is a true statement, lol
     def generate(site)
       Dir.mkdir('scripts') unless Dir.exists?('scripts')
-      
+
       open('scripts/PrecinctInfo.js', 'w') do |file|
         file << open('http://www.canyonco.org/CanyonCounty/Webparts/Elections/js/PrecinctInfo.js?v2').read
       end
