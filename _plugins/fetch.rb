8c766bf56f6361fc31304f4401831d8416152660
Kenneth Wilcox <wilcoxk@gmail.com>
2014-10-06 09:15:53 -0600

    Fetching middle tier data now

--- _plugins/fetch.rb
+++ _plugins/fetch.rb
@@ -6,10 +6,20 @@ module Jekyll
     safe true # not sure if this is a true statement, lol
     def generate(site)
       Dir.mkdir('scripts') unless Dir.exists?('scripts')
+      Dir.mkdir('_data') unless Dir.exists?('_data')
+      Dir.mkdir('_data/jailRoster') unless Dir.exists?('_data/jailRoster')
 
       open('scripts/PrecinctInfo.js', 'w') do |file|
         file << open('http://www.canyonco.org/CanyonCounty/Webparts/Elections/js/PrecinctInfo.js?v2').read
       end
+
+      open('_data/arrests.json', 'w') do |file|
+        file << open('http://secret.canyonco.org/Sheriff/CurrentArrest').read
+      end
+
+      open('_data/jailList.json', 'w') do |file|
+        file << open('http://secret.canyonco.org/Sheriff/JailList').read
+      end      
     end
   end
 end
