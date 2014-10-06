e6bce5dbedcee3a07d9203cc44aa8a12954ee47c
Kenneth Wilcox <wilcoxk@gmail.com>
2014-10-06 12:24:41 -0600

    added some exception handling...

--- _plugins/fetch.rb
+++ _plugins/fetch.rb
@@ -5,21 +5,31 @@ module Jekyll
   class FetchGenerator < Generator
     safe true # not sure if this is a true statement, lol
     def generate(site)
-      Dir.mkdir('scripts') unless Dir.exists?('scripts')
-      Dir.mkdir('_data') unless Dir.exists?('_data')
-      Dir.mkdir('_data/jailRoster') unless Dir.exists?('_data/jailRoster')
-
-      open('scripts/PrecinctInfo.js', 'w') do |file|
-        file << open('http://www.canyonco.org/CanyonCounty/Webparts/Elections/js/PrecinctInfo.js?v2').read
+      begin
+        Dir.mkdir('scripts') unless Dir.exists?('scripts')
+        Dir.mkdir('_data') unless Dir.exists?('_data')
+        Dir.mkdir('_data/jailRoster') unless Dir.exists?('_data/jailRoster')
+      rescue Exception => msg
+        puts "Error creating directories"
+        puts msg
       end
 
-      open('_data/arrests.json', 'w') do |file|
-        file << open('http://secret.canyonco.org/Sheriff/CurrentArrest').read
-      end
+      begin
+        open('scripts/PrecinctInfo.js', 'w') do |file|
+          file << open('http://www.canyonco.org/CanyonCounty/Webparts/Elections/js/PrecinctInfo.js?v2').read
+        end
 
-      open('_data/jailList.json', 'w') do |file|
-        file << open('http://secret.canyonco.org/Sheriff/JailList').read
-      end      
+        open('_data/arrests.json', 'w') do |file|
+          file << open('http://secret.canyonco.org/Sheriff/CurrentArrest').read
+        end
+
+        open('_data/jailList.json', 'w') do |file|
+          file << open('http://secret.canyonco.org/Sheriff/JailList').read
+        end      
+      rescue Exception => msg
+        puts "Error downloading content"
+        puts msg
+      end
     end
   end
 end
